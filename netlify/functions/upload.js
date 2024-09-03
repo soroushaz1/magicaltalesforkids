0336bad01e2da79424e9d382da35db058eab5753
Soroush Amel <soroushzendedel1@gmail.com>
2024-09-03 14:22:59 +0330

    Upload button testing 2

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -1,20 +1,7 @@
-const multer = require('multer');
-const upload = multer({ dest: '/tmp/uploads' });
-
 exports.handler = async (event, context) => {
-  return new Promise((resolve, reject) => {
-    upload.single('file')(event, {}, (err) => {
-      if (err) {
-        resolve({
-          statusCode: 500,
-          body: JSON.stringify({ message: 'File upload failed.' }),
-        });
-      } else {
-        resolve({
-          statusCode: 200,
-          body: JSON.stringify({ message: 'File uploaded successfully!' }),
-        });
-      }
-    });
-  });
-};
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Function is working!' }),
+    };
+  };
+  
